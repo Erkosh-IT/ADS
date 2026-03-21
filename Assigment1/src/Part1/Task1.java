96ea89f5e16d131aa2df6f3271f019c2b3bc5364
Erkebulan Korganbek <254420@astanait.edu.kz>
2026-03-21 23:18:44 +0500

    123

--- Assigment1/src/Part1/Task1.java
+++ Assigment1/src/Part1/Task1.java
@@ -1,4 +1,18 @@
 package Part1;
-
+import java.util.Scanner;
 public class Task1 {
+    public static void print(int n) {
+        n = Math.abs(n);
+        if (n < 10) {
+            System.out.println(n);
+        } else {
+            print(n / 10);
+            System.out.println(n % 10);
+        }
+    }
+    public  static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        print(number);
+    }
 }
